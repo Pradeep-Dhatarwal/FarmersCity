37eb6048b034104ec45e4b56c637b64bee551898
Pr4d33p_d <pradsngh523@gmail.com>
2020-10-19 14:56:30 +0530

    first commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const passportLocalMongoose           = require("passport-local-mongoose");
 const mongoose                        = require("mongoose");
 const flash                           = require("connect-flash");
 const app                             = express();
-// require('dotenv').config();
+require('dotenv').config();
 mongoose.connect( process.env.MONGO_URI , {useNewUrlParser: true , useUnifiedTopology: true },(err)=>{
   if(!err){
     console.log("Database connected successfully")
