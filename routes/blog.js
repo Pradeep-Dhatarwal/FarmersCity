111e81cc870f6d70644f5edd468b7a03bad9f2bb
Pradeep Dhatarwal <pradsngh523@gmail.com>
2021-02-16 16:31:31 +0530

    bugfix for error 500

--- routes/blog.js
+++ routes/blog.js
@@ -10,8 +10,13 @@ const storage = multer.diskStorage({
 		// console.log(file);
 		cb(null, "public/uploads");
 	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname);
+		filename: function (req, file, cb) {
+			console.log(file);
+			if (fs.existsSync(path.join("public/uploads",file.originalname))) {
+				cb(null, file.originalname+"(1)");
+			}else{
+				cb(null, file.originalname);
+			}
 	}
 });
 const upload = multer({ storage: storage });
